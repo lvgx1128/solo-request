234c1e00e1c1413f34bc2a3d8b79fd3111d4f14a
吕广新 <guangxin.lv@china.zhaogang.com>
2022-08-29 19:41:50 +0800

    feat: 更新版本

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,2 +1,11 @@
 export { initInstance, post, get } from './instance';
 export { useAxios } from './useAxios';
+import { initInstance, post, get } from './instance';
+import { useAxios } from './useAxios';
+declare const _default: {
+    initInstance: typeof initInstance;
+    post: typeof post;
+    get: typeof get;
+    useAxios: typeof useAxios;
+};
+export default _default;
--- packages/types/instance.d.ts
+++ packages/types/instance.d.ts
@@ -1,4 +1,9 @@
 import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+declare type TypeAipResponse = {
+    code?: number;
+    data?: any;
+    [key: string]: any;
+};
 export declare function initInstance(request?: (req: AxiosRequestConfig) => AxiosRequestConfig, response?: (res: AxiosResponse) => AxiosResponse): void;
 /**
  * @param {string} url
@@ -6,5 +11,6 @@ export declare function initInstance(request?: (req: AxiosRequestConfig) => Axio
  * @param {*} [config] config.intact是否是完整的url
  * @returns {Promise<T>}
  */
-export declare function post(url: string, data?: Record<string, any>, conf?: Record<string, any>): Promise<AxiosResponse>;
-export declare function get(url: string, data?: Record<string, any>, conf?: Record<string, any>): Promise<AxiosResponse>;
+export declare function post(url: string, data?: Record<string, any>, conf?: Record<string, any>): Promise<TypeAipResponse>;
+export declare function get(url: string, data?: Record<string, any>, conf?: Record<string, any>): Promise<TypeAipResponse>;
+export {};
